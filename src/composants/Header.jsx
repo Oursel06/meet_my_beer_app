4349eec033d52dd74d2b7783a076393e6c28cf6b
Oursel06 <alexoursel6@gmail.com>
2023-03-20 15:46:39 +0100

    configuration projet

---
+++ src/composants/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import biereheadshot from "../image/biereheadshot.png"
+
+function Header() {
+    return (
+        <div>
+            <img src={biereheadshot} alt="biere"></img>
+            <p>test</p>
+        </div>
+    );
+}
+
+export default Header;
