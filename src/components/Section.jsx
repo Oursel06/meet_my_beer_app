8185b0b4097c2c4400c8644dea565a8eb880d5c1
BlApCa <fleming.duffard@gmail.com>
2023-05-02 15:14:33 +0200

    Added Section component + css

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,11 +1,9 @@
-import Section from './Section.jsx'
-
 function Screen(Props) {
     let test = 'testLol'
 
     return (
-        <div className="mainScreen">
-            <Section />
+        <div className="section">
+            <div class="cube"></div>
         </div>
     )
 }
