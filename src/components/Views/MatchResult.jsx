40bce7b51c870e151b2cc1c76a1c57684144592f
BlApCa <fleming.duffard@gmail.com>
2023-05-22 14:15:36 +0200

    Implementing algorithm to calculate matching beer and return it

--- src/components/Views/MatchResult.jsx
+++ src/components/Views/MatchResult.jsx
@@ -3,42 +3,46 @@ import {useState, useEffect} from 'react'
 function MatchResult({beerData,userChoices}) {
 
     const [data, setData] = useState(userChoices)
-    const [dreamBeer,setDreamBeer] = useState({})
+    const [match,setMatch] = useState([])
+
+
+    function roundToTwo(num) {
+        return +(Math.round(num + "e+2")  + "e-2");
+    }
+    const graduation = (e) => {
+        const notes = []
+        console.log(data[0])
+        e.couleurs[0] != data[0].type.value ? notes.push(roundToTwo(6*(1-data[0].type.weight/10))) : notes.push(10)
+        e.saveurs[0] != data[0].gout.value ? notes.push(roundToTwo(6*(1-data[0].gout.weight/10))) : notes.push(10)
+        e.amertume != data[0].amertume.value ? notes.push(roundToTwo(6*(1-data[0].amertume.weight/10))) : notes.push(10)
+        let degres = ''
+        e.nbDegres < 5 ? degres = 'Faible' : degres < 8 ? degres = 'Moyen' : degres = 'Fort'  
+        degres !== data[0].degres.value ? notes.push(roundToTwo(6*(1-data[0].degres.weight/10))) : notes.push(10)
+
+        return notes
+    }
 
     useEffect(()=>{
-        data.forEach(e =>{
-            setDreamBeer({
-                type:{
-                    value:e.type.value[0],
-                    weight:e.type.weight[0]
-                },
-                gout:{
-                    value:e.gout.value[0],
-                    weight:e.gout.weight[0]
-                },
-                amertume:{
-                    value:e.amertume.value[0],
-                    weight:e.amertume.weight[0]
-                },
-                degres:{
-                    value:e.degres.value[0],
-                    weight:e.degres.weight[0]
-                }
-            })
+        console.log(data)
+        let winner = ''
+        beerData.forEach(e => {
+            e['notes'] = graduation(e)
+            console.log(e)
+            e['score'] = e.notes[0]+e.notes[1]+e.notes[2]+e.notes[3]/10
+            winner == '' ? winner = e : winner.score < e.score ? winner = e : winner = winner
         })
+        setMatch([
+            <div>
+                {winner.nom}
+            </div>
+        ])
     },[data])
-
-    useEffect(()=> {
-        console.log(dreamBeer)
-        console.log(beerData)
-    },[dreamBeer])
-
 
 
     return (
         <div >
             It's a match !
-            
+            {match}
         </div>
     )
 }
